aed32a41b1726982a3a60dfaa3623f75c3e0a09c
Goldsucher07 <60298474+Goldsucher07@users.noreply.github.com>
2022-08-28 14:25:45 +0200

    Uploadet existing Files
    Neue Files halt nh

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+// Credits to expl0it, shellcode.team
+// GitHub: https://github.com/Lumm1t/obnoxious.club
+
+class _app {
+    id = 0;
+    videoElement = null;
+    audioElement = null;
+    musicVolume = 0.12;
+    musicFadeIn = 4000;
+    skippedIntro = false;
+    backgroundToggler = false;
+    shouldIgnoreVideo = false;
+    effects = [
+        "bounce",
+        "flash",
+        "pulse",
+        "rubberBand",
+        "shake",
+        "swing",
+        "tada",
+        "wobble",
+        "jello",
+    ];
+    brandDescription = [
+        "Gamer",
+        "trashtalker",
+        "full time money waster",
+        "Contact: Goldsucher07#7153",
+    ];
+
+    titleChanger = (text, delay) => {
+        if (!text) return;
+
+        delay = delay || 2000;
+
+        let counter = 0;
+
+        setInterval(() => {
+            if (counter < text.length) document.title = text[counter++];
+            else document.title = text[(counter = 0)];
+        }, delay);
+    };
+
+    iconChanger = (urls, delay) => {
+        if (!urls) return;
+
+        delay = delay || 2000;
+
+        let counter = 0;
+
+        setInterval(() => {
+            if (counter < urls.length) {
+                const link =
+                    document.querySelector("link[rel*='icon']") ||
+                    document.createElement("link");
+
+                link.type = "image/x-icon";
+                link.rel = "shortcut icon";
+                link.href = urls[counter];
+
+                document.getElementsByTagName("head")[0].appendChild(link);
+            } else counter = 0;
+
+            ++counter;
+        }, delay);
+    };
+}
+
+const app = new _app();
